e94566ed09fa979feb3c7061efb0981b189954cf
Aanchal-kumari02 <aanchal.kumari193@gmail.com>
2024-06-24 13:33:53 +0530

    Project-1 created

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, facere!</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
